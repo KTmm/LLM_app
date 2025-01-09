713cd59bd25aa5750de4cfc178904467401d458e
Ying Zhou <yzhou@workforcesoftware.com>
2025-01-09 00:29:54 -0500

    Add validation part and refactor methods

--- backend_api/chat_summarizer.py
+++ backend_api/chat_summarizer.py
@@ -1,62 +1,61 @@
 from anthropic import AsyncAnthropic
-import time
 import asyncio
 import os
 import base64
+import sys
 
-system_prompt_template = """You are {name} a virtual assistant created by {owner}. Today is {date}. You provide responses to questions that are clear, straightforward, and factually accurate, without speculation or falsehood. Given the following context, please answer each question truthfully to the best of your abilities based on the provided information. Answer each question with a brief summary followed by several bullet points. 
+### This script first reads a research paper, summarize the key experiement, then check if the summarzied key experiments have been repeated or conducted in the second paper
 
-Example:
-Summary of answer
-- bullet point 1
-- bullet point 2
-...
-
-<context>
-{context}
-</context>
-"""
-
-with open("elife-26403.pdf",'rb') as in_file:
-    context_content = in_file.read()
-
-system_prompt = system_prompt_template.format(
-    name="Research Paper Analyzer",
-    owner="Ying",
-    date="Dec. 20th, 2024",
-    context=context_content
-)
-
-async def chat_func():
+def main():
     client = AsyncAnthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
+    param1 = sys.argv[1] #This is the target paper we are going to validate
+    param2 = sys.argv[2] #This is the paper we would like use to validate
 
+    target_paper = read_pdf_file(param1)
+    paper_use_for_check = read_pdf_file(param2)
+
+    summarize_prompt = "Analyze the research paper and summarize the key experiments. The summarization should include detailed experiment condition, and experiment result"
+
+    summary = asyncio.run(analyze(client, target_paper, summarize_prompt))
+
+    validation_prompt = "Please validate the following key experiment has been conducted or validated in the uploaded research paper:" + summary
+
+    validation = asyncio.run(analyze(client, paper_use_for_check, validation_prompt))
+     
+
+#Read pdf file from given file path
+def read_pdf_file(file_path):
+    with open(file_path,'rb') as in_file:
+        pdf_content = in_file.read()
+    return pdf_content
+
+#This method calls the arthropic API to analyze a research paper
+async def analyze(client, paper, prompt):
     response = await client.messages.create(
         model="claude-3-5-sonnet-20241022",
         max_tokens=8192,
         temperature=0,
-      #  system=system_prompt,
         messages=[
             {"role": "user", "content": [{
                                             "type": "document",
                                             "source":  {
                                                 "type": "base64",
                                                 "media_type": "application/pdf",
-                                                "data": base64.b64encode(context_content).decode()
+                                                "data": base64.b64encode(paper).decode()
                                             }},
                                           {
                                             "type": "text",
-                                            "text": "Analyze the research paper and summarize the key experiments. The summarization would include detailed experiment condition, and experiment result"  
+                                            "text": prompt
                                           }]}
         ],
         stream=True
     )
+    response_str = ""
     async for message in response:
         if message.type =="content_block_delta":
             print(message.delta.text, end="", flush=True)
+            response_str += message.delta.text
+    return response_str        
 
-   # print(result.content)
-    # async for message in result:
-    #     print(message.content, flush=True, end="")
-
-
-asyncio.run(chat_func())
+if __name__ == "__main__":
+    main()
